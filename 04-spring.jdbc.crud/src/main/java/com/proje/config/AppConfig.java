e63fee309523f148d9e72f414f5cd53d0a9aa093
Beşir AYDEMİR <javahesaphesap@gmail.com>
2022-09-17 13:51:01 +0300

    Anataion ile çalışma not java version 1.8 olmaldır

--- 04-spring.jdbc.crud/src/main/java/com/proje/config/AppConfig.java
+++ 04-spring.jdbc.crud/src/main/java/com/proje/config/AppConfig.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.stereotype.Component;
-//@Configuration
-//@ComponentScan("com.proje")
+
+@Configuration
+@ComponentScan("com.proje")
 public class AppConfig {
 	
-	/*@Bean
+	@Bean
 	public DataSource dataSource() {
 	
 	  DriverManagerDataSource databasesurce=new DriverManagerDataSource();
 	  databasesurce.setDriverClassName("com.mysql.jdbc.Driver");
-	  databasesurce.setUrl("jdbc:mysql://localhost:3306/spring_jdbc");
+	  databasesurce.setUrl("jdbc:mysql://localhost:3306/spring_jbc");
 	  databasesurce.setUsername("root");
 	  databasesurce.setPassword("");
 	  
 	  
 	  return databasesurce;
-	}*/
+	}
 }
