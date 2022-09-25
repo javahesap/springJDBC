d12e14bcfe4d9e9cabc04d68c3f207737e8974e4
Beşir AYDEMİR <javahesaphesap@gmail.com>
2022-09-26 00:18:28 +0300

    kodlama sonrası

--- 09-spring.jdbc.transaction/src/main/java/com/proje/service/impl/UserDetailServiceIpl.java
+++ 09-spring.jdbc.transaction/src/main/java/com/proje/service/impl/UserDetailServiceIpl.java
@@ -1,6 +1,41 @@
 package com.proje.service.impl;
 
-public class UserDetailServiceIpl  {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proje.model.UserDetail;
+import com.proje.repostory.UserDetailRepostory;
+import com.proje.service.UserDetailService;
+@Service
+public class UserDetailServiceIpl implements UserDetailService {
+         @Autowired
+	   private UserDetailRepostory userdetailRepository;
+	
+	@Override
+	public boolean Save(int userId, UserDetail userdetail) {
+		// TODO Auto-generated method stub
+		return userdetailRepository.Save(userId, userdetail);
+	}
+
+	@Override
+	public boolean update(UserDetail userDetail) {
+		// TODO Auto-generated method stub
+		return userdetailRepository.update(userDetail);
+	}
+
+	@Override
+	public UserDetail userDetailfindById(int id) {
+		// TODO Auto-generated method stub
+		return userdetailRepository.userDetailfindById(id);
+	}
+
+	@Override
+	public List<UserDetail> findUserDetails() {
+		// TODO Auto-generated method stub
+		return userdetailRepository.findUserDetails();
+	}
 
 	
 	
